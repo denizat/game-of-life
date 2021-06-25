4b6ba8c9707545a268b8923e7145725dd181875b
denizat <deniza.telci@gmail.com>
2021-06-25 18:41:50 -0500

    Random life

--- src/Life.ts
+++ src/Life.ts
@@ -1,9 +1,17 @@
 type Dead = 0
 type Alive = 1
-export type Planet = Array<Array<Dead | Alive>>
+export type Land = Array<Array<Dead | Alive>>
 
 class World {
-    land: Planet = []
+    // Copies a planet by value
+
+
+}
+
+export class Life {
+
+    land: Land = []
+
     constructor(max: number) {
         for (let i = 0; i < max; i++) {
             this.land.push([])
@@ -12,28 +20,6 @@ class World {
             }
         }
     }
-
-    showLand() {
-        let out = ""
-        this.land.forEach((line) => {
-            out += line.join(" ") + "=\n"
-        })
-        console.log(out)
-    }
-
-    // Copies a planet by value
-    copy() {
-        let out: Planet = []
-        this.land.forEach((line, x) => {
-            out.push([])
-            line.forEach((thing) => {
-                out[x].push(thing)
-            })
-
-        });
-        return out
-    }
-
     neighbors(x: number, y: number) {
         let alive = 0
         for (let i = x - 1; i < x + 2; i++) {
@@ -62,99 +48,34 @@ class World {
 
         }
     }
+    copy() {
+        let out: Land = []
+        this.land.forEach((line, x) => {
+            out.push([])
+            line.forEach((thing) => {
+                out[x].push(thing)
+            })
 
-}
-
-export class Life {
-
-    world: World
-
-    constructor(max: number) {
-        this.world = new World(max)
-    }
-    addPerimeter(p: Planet): void {
-        // Add a line of zeros to top and bottom
-        p.forEach(l => {
-            l.unshift(0)
-            l.push(0)
-        })
-        // Add line of zeros to sides
-        let line = p[0]
-        line.forEach(i => i = 0)
-        p.unshift(line)
-        p.push(line)
-    }
-    removePerimeter(p: Planet): void {
-        // Cut off first column
-        let cut = true
-        p[0].forEach(i => {
-            if (i === 1) {
-                cut = false
-            }
-        })
-        if (cut) {
-            p.shift()
-        }
-        // Cut off last column
-        cut = true
-        p[p.length - 1].forEach(i => {
-            if (i === 1) {
-                cut = false
-            }
-        })
-        if (cut) {
-            p.pop()
-        }
-        // // Cut off top and bottom
-        // let cutTop = true
-        // let cutBottom = true
-        // for (let i = 0; i < p.length; i++) {
-        //     if (p[i][0] === 1) {
-        //         cutTop = false
-        //     }
-        //     if (p[i][p[i].length - 1] === 1) {
-        //         cutBottom = false
-        //     }
-        // }
-        // if (cutTop) {
-        //     for (let i = 0; i < p.length; i++) {
-        //         p[i].shift()
-        //     }
-        // }
-        // if (cutBottom) {
-        //     for (let i = 0; i < p.length; i++) {
-        //         p[i].pop()
-        //     }
-        // }
+        });
+        return out
     }
     iterate() {
-        let nextState = this.world.copy()
-        // this.addPerimeter(nextState)
-        this.world.land.forEach((line, x) => {
+        let nextState = this.copy()
+        this.land.forEach((line, x) => {
             line.forEach((thing, y) => {
-                // Thing is alive
                 if (thing === 1) {
-                    let n = this.world.neighbors(x, y)
+                    let n = this.neighbors(x, y)
                     if (!(n === 2 || n === 3)) {
                         nextState[x][y] = 0
                     }
                 } else {
-                    if (this.world.neighbors(x, y) === 3) {
+                    if (this.neighbors(x, y) === 3) {
                         nextState[x][y] = 1
                     }
                 }
             })
-            this.showWorld()
         })
-        // this.removePerimeter(nextState)
-        this.world.land = nextState
+        this.land = nextState
     }
 
-    showWorld() {
-        this.world.showLand()
-    }
-
-    randomize() {
-        this.world.randomize()
-    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css"
-import { Life, Planet } from "./Life"
+import { Life, Land } from "./Life"
 
-class App extends React.Component<{}, { land: Planet }> {
+function sleep(ms: any) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+class App extends React.Component<{}, { land: Land }> {
     life: Life
-    constructor() {
+    constructor({ }) {
         super({})
         this.life = new Life(10)
         this.state = {
-            land: this.life.world.land
+            land: this.life.copy()
         }
     }
-    componentDidMount() {
-        this.life.iterate()
+    async componentDidMount() {
+        await this.life.randomize()
+
+
         this.setState({
-            land: this.life.world.land
+            land: this.life.land
         })
+        await sleep(10)
+        this.forceUpdate()
 
     }
     render() {
-        this.life.randomize()
-        this.setState({
-            land: this.life.world.land
-        })
         return (
-            this.state.land.map((v) => { return (<div>{v}</div>) })
+            this.state.land.map((v) => { return (<div className="flex flex-row">{v.map(e => { return (<div className={`w-5 h-5 ${e ? "bg-black" : null}`}></div>) })} </div>) })
         )
     }
 }
